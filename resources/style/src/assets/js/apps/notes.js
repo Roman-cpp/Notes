84b0f6df45fa65c3e3c1173800204e3e6d038a22
romik <rom@ya>
2022-10-16 19:11:49 +0300

    commit

--- resources/style/src/assets/js/apps/notes.js
+++ resources/style/src/assets/js/apps/notes.js
@@ -1,45 +1,47 @@
+let requestURL = 'project';
+
 $(document).ready(function() {
     function deleteNote() {
         $(".delete-note").off('click').on('click', function(event) {
-          event.stopPropagation();
-          $(this).parents('.note-item').remove();
+            event.stopPropagation();
+            $(this).parents('.note-item').remove();
         })
     }
 
     function favNote() {
         $(".fav-note").off('click').on('click', function(event) {
-          event.stopPropagation();
-          $(this).parents('.note-item').toggleClass('note-fav');
+            event.stopPropagation();
+            $(this).parents('.note-item').toggleClass('note-fav');
         })
     }
 
     function addLabelGroups() {
         $('.tags-selector .label-group-item').off('click').on('click', function(event) {
-          event.preventDefault();
-          /* Act on the event */
-          var getclass = this.className;
-          var getSplitclass = getclass.split(' ')[0];
-          if ($(this).hasClass('label-personal')) {
-            $(this).parents('.note-item').removeClass('note-social');
-            $(this).parents('.note-item').removeClass('note-work');
-            $(this).parents('.note-item').removeClass('note-important');
-            $(this).parents('.note-item').toggleClass(getSplitclass);
-          } else if ($(this).hasClass('label-work')) {
-            $(this).parents('.note-item').removeClass('note-personal');
-            $(this).parents('.note-item').removeClass('note-social');
-            $(this).parents('.note-item').removeClass('note-important');
-            $(this).parents('.note-item').toggleClass(getSplitclass);
-          } else if ($(this).hasClass('label-social')) {
-            $(this).parents('.note-item').removeClass('note-personal');
-            $(this).parents('.note-item').removeClass('note-work');
-            $(this).parents('.note-item').removeClass('note-important');
-            $(this).parents('.note-item').toggleClass(getSplitclass);
-          } else if ($(this).hasClass('label-important')) {
-            $(this).parents('.note-item').removeClass('note-personal');
-            $(this).parents('.note-item').removeClass('note-social');
-            $(this).parents('.note-item').removeClass('note-work');
-            $(this).parents('.note-item').toggleClass(getSplitclass);
-          }
+            event.preventDefault();
+            /* Act on the event */
+            var getclass = this.className;
+            var getSplitclass = getclass.split(' ')[0];
+            if ($(this).hasClass('label-personal')) {
+                $(this).parents('.note-item').removeClass('note-social');
+                $(this).parents('.note-item').removeClass('note-work');
+                $(this).parents('.note-item').removeClass('note-important');
+                $(this).parents('.note-item').toggleClass(getSplitclass);
+            } else if ($(this).hasClass('label-work')) {
+                $(this).parents('.note-item').removeClass('note-personal');
+                $(this).parents('.note-item').removeClass('note-social');
+                $(this).parents('.note-item').removeClass('note-important');
+                $(this).parents('.note-item').toggleClass(getSplitclass);
+            } else if ($(this).hasClass('label-social')) {
+                $(this).parents('.note-item').removeClass('note-personal');
+                $(this).parents('.note-item').removeClass('note-work');
+                $(this).parents('.note-item').removeClass('note-important');
+                $(this).parents('.note-item').toggleClass(getSplitclass);
+            } else if ($(this).hasClass('label-important')) {
+                $(this).parents('.note-item').removeClass('note-personal');
+                $(this).parents('.note-item').removeClass('note-social');
+                $(this).parents('.note-item').removeClass('note-work');
+                $(this).parents('.note-item').toggleClass(getSplitclass);
+            }
         });
     }
 
@@ -58,17 +60,17 @@ $(document).ready(function() {
 
     var $btns = $('.list-actions').click(function() {
         if (this.id == 'all-notes') {
-          var $el = $('.' + this.id).fadeIn();
-          $('#ct > div').not($el).hide();
+            var $el = $('.' + this.id).fadeIn();
+            $('#ct > div').not($el).hide();
         } if (this.id == 'important') {
-          var $el = $('.' + this.id).fadeIn();
-          $('#ct > div').not($el).hide();
+            var $el = $('.' + this.id).fadeIn();
+            $('#ct > div').not($el).hide();
         } else {
-          var $el = $('.' + this.id).fadeIn();
-          $('#ct > div').not($el).hide();
+            var $el = $('.' + this.id).fadeIn();
+            $('#ct > div').not($el).hide();
         }
         $btns.removeClass('active');
-        $(this).addClass('active');  
+        $(this).addClass('active');
     })
 
     $('#btn-add-notes').on('click', function(event) {
@@ -90,42 +92,51 @@ $(document).ready(function() {
         var $_noteTitle = document.getElementById('n-title').value;
         var $_noteDescription = document.getElementById('n-description').value;
 
+        //send server
+        const params = "title=" + $_noteTitle+ "&description=" + $_noteDescription;
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', requestURL);
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhr.setRequestHeader("X-CSRF-Token", document.querySelector('meta[name="csrf-token"]').content)
+        xhr.send(params);
+
+
         $html = '<div class="note-item all-notes">' +
-                    '<div class="note-inner-content">' +
-                        '<div class="note-content">' +
-                            '<p class="note-title" data-noteTitle="'+$_noteTitle+'">'+$_noteTitle+'</p>' +
-                            '<p class="meta-time">'+today+'</p>' +
-                            '<div class="note-description-content">' +
-                                '<p class="note-description" data-noteDescription="'+$_noteDescription+'">'+$_noteDescription+'</p>' +
-                            '</div>' +
-                        '</div>' +
-                        '<div class="note-action">' +
-                            '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-star fav-note"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon></svg> ' +
-                            '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2 delete-note"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>' +
-                        '</div>' +
-                        '<div class="note-footer">' +
-                            '<div class="tags-selector btn-group">' +
-                                '<a class="nav-link dropdown-toggle d-icon label-group" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="true">' +
-                                    '<span>' +
-                                        '<div class="tags">' +
-                                            '<div class="g-dot-personal"></div>' +
-                                            '<div class="g-dot-work"></div>' +
-                                            '<div class="g-dot-social"></div>' +
-                                            '<div class="g-dot-important"></div>' +
-                                            '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-vertical"><circle cx="12" cy="12" r="1"></circle><circle cx="12" cy="5" r="1"></circle><circle cx="12" cy="19" r="1"></circle></svg>' +
-                                        '</div>' +
-                                    '</span>' +
-                                '</a>' +
-                                '<div class="dropdown-menu dropdown-menu-right d-icon-menu">' +
-                                    '<a class="note-personal label-group-item label-personal dropdown-item position-relative g-dot-personal" href="javascript:void(0);"> Personal</a>' +
-                                    '<a class="note-work label-group-item label-work dropdown-item position-relative g-dot-work" href="javascript:void(0);"> Work</a>' +
-                                    '<a class="note-social label-group-item label-social dropdown-item position-relative g-dot-social" href="javascript:void(0);"> Social</a>' +
-                                    '<a class="note-important label-group-item label-important dropdown-item position-relative g-dot-important" href="javascript:void(0);"> Important</a>' +
-                                '</div>' +
-                            '</div>' +
-                        '</div>' +
-                    '</div>' +
-                '</div> ';
+            '<div class="note-inner-content">' +
+            '<div class="note-content">' +
+            '<p class="note-title" data-noteTitle="'+$_noteTitle+'">'+$_noteTitle+'</p>' +
+            '<p class="meta-time">'+today+'</p>' +
+            '<div class="note-description-content">' +
+            '<p class="note-description" data-noteDescription="'+$_noteDescription+'">'+$_noteDescription+'</p>' +
+            '</div>' +
+            '</div>' +
+            '<div class="note-action">' +
+            '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-star fav-note"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon></svg> ' +
+            '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2 delete-note"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>' +
+            '</div>' +
+            '<div class="note-footer">' +
+            '<div class="tags-selector btn-group">' +
+            '<a class="nav-link dropdown-toggle d-icon label-group" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="true">' +
+            '<span>' +
+            '<div class="tags">' +
+            '<div class="g-dot-personal"></div>' +
+            '<div class="g-dot-work"></div>' +
+            '<div class="g-dot-social"></div>' +
+            '<div class="g-dot-important"></div>' +
+            '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-vertical"><circle cx="12" cy="12" r="1"></circle><circle cx="12" cy="5" r="1"></circle><circle cx="12" cy="19" r="1"></circle></svg>' +
+            '</div>' +
+            '</span>' +
+            '</a>' +
+            '<div class="dropdown-menu dropdown-menu-right d-icon-menu">' +
+            '<a class="note-personal label-group-item label-personal dropdown-item position-relative g-dot-personal" href="javascript:void(0);"> Personal</a>' +
+            '<a class="note-work label-group-item label-work dropdown-item position-relative g-dot-work" href="javascript:void(0);"> Work</a>' +
+            '<a class="note-social label-group-item label-social dropdown-item position-relative g-dot-social" href="javascript:void(0);"> Social</a>' +
+            '<a class="note-important label-group-item label-important dropdown-item position-relative g-dot-important" href="javascript:void(0);"> Important</a>' +
+            '</div>' +
+            '</div>' +
+            '</div>' +
+            '</div>' +
+            '</div> ';
 
         $("#ct").prepend($html);
         $('#notesMailModal').modal('hide');
@@ -157,10 +168,10 @@ getNoteTitleInput.addEventListener('input', function() {
     getNoteTitleInputValue = this.value;
 
     if (getNoteTitleInputValue == "") {
-      $_getValidationField[0].innerHTML = 'Title Required';
-      $_getValidationField[0].style.display = 'block';
+        $_getValidationField[0].innerHTML = 'Title Required';
+        $_getValidationField[0].style.display = 'block';
     } else {
-      $_getValidationField[0].style.display = 'none';
+        $_getValidationField[0].style.display = 'none';
     }
 })
 
@@ -168,13 +179,13 @@ getNoteDescriptionInput = document.getElementById('n-description');
 
 getNoteDescriptionInput.addEventListener('input', function() {
 
-  getNoteDescriptionInputValue = this.value;
+    getNoteDescriptionInputValue = this.value;
 
-  if (getNoteDescriptionInputValue == "") {
-    $_getValidationField[1].innerHTML = 'Description Required';
-    $_getValidationField[1].style.display = 'block';
-  } else {
-    $_getValidationField[1].style.display = 'none';
-  }
+    if (getNoteDescriptionInputValue == "") {
+        $_getValidationField[1].innerHTML = 'Description Required';
+        $_getValidationField[1].style.display = 'block';
+    } else {
+        $_getValidationField[1].style.display = 'none';
+    }
 
 })
